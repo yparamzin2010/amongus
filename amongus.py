e266a9bb3b01a254a43d4eb194336b2c1af56d7d
yparamzin2010 <59018601+yparamzin2010@users.noreply.github.com>
2023-08-28 16:50:41 +1000

    Create amongus.py
    sussy

---
+++ amongus.py
@@ -0,0 +1,27 @@
+from flask import Flask
+import random
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+  # Generate a random color.
+  color = random.choice(["red", "green", "blue", "yellow", "purple"])
+
+  # Create the HTML for the square.
+  html = """
+  <html>
+    <head>
+      <title>My Website</title>
+    </head>
+    <body>
+      <div style="width: 100px; height: 100px; background-color: {color};"></div>
+    </body>
+  </html>
+  """.format(color=color)
+
+  # Return the HTML.
+  return html
+
+if __name__ == "__main__":
+  app.run(debug=True)
